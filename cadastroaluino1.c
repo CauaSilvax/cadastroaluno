642640eca399db621c1f8ca05e8daa5d07267a66
Cauã Silva <caua.guaratiba@gmail.com>
2024-10-08 11:32:16 -0300

    Update cadastroaluino1.c

--- cadastroaluino1.c
+++ cadastroaluino1.c
@@ -6,27 +6,39 @@ int gerarSenha() {
     return rand() % 100 + 1; 
 }
 
-// Função principal
+// main
 int main() {
     char resposta;
     int guiche = 1; 
     
-   
-    srand(time(NULL));
+    srand(time(NULL));  //gerador de números aleatórios
     
     do {
         printf("Deseja retirar uma senha? (s ou n): ");
         scanf(" %c", &resposta);
         
         if (resposta == 's' || resposta == 'S') {
+            char tipo;
             int senha = gerarSenha();
-            printf("Senha: %d | Guichê: %d\n", senha, guiche);
-            guiche++; 
+            
+            printf("Tipo de paciente (c para comum, p para preferencial): ");
+            scanf(" %c", &tipo);
+            
+            if (tipo == 'c' || tipo == 'C') {
+                printf("Senha: C%d | Guichê: %d\n", senha, guiche);
+            } else if (tipo == 'p' || tipo == 'P') {
+                printf("Senha: P%d | Guichê: %d\n", senha, guiche);
+            } else {
+                printf("Tipo de paciente inválido.\n");
+                continue;  
+            }
+            
+            guiche++;  
         }
         
     } while (resposta == 's' || resposta == 'S'); 
     
-    printf("obrigado!\n");
+    printf("Obrigado!\n");
     
     return 0;
 }
